43aa91978c0fdf5fe252ce3b0900c49117fa1b24
phuchieuct93 <phuchieuct93@gmail.com>
2015-01-25 15:42:47 +0000

    Rejected commit: Default

--- News/app/src/main/java/com/activity/Splash.java
+++ News/app/src/main/java/com/activity/Splash.java
@@ -4,12 +4,19 @@ package com.activity;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Handler;
+import android.util.Log;
 
+import com.feed.Feed;
 import com.phuchieu.news.R;
+import com.services.CategoryService_JSON;
 import com.services.FeedService;
 
 import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Background;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.UiThread;
+
+import java.util.List;
 
 @EActivity(R.layout.splash)
 public class Splash extends Activity {
@@ -28,6 +35,8 @@ public class Splash extends Activity {
                 Splash.this.finish();
             }
         }, SPLASH_DISPLAY_LENGTH);
+        //test();
+
     }
 
 }
